08995c04dd431f0e92e04bb6e8d1b994803cc8a8
NATE JOHNSON <42559281+Elipticblock5@users.noreply.github.com>
2021-03-06 14:23:42 -0700

    changing bootstrap for now, ids are timeXX, SavedItems, and saveButton for grid

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -6,40 +6,31 @@
  $("#currentDay").html(dateNow);
  
  
+ $(document).ready(function () {
+    // saveButton listening 
+
+    $(".saveButton").on("click", function () {
+        
+        var textNow = $(this).siblings(".scheduleItems").val();  
+        var timeNow = $(this).parent().attr("id");
+
+        // Save text in local storage
+        localStorage.setItem(timeNow, textNow);
+    })
  
  
+  
  
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
+  
  
  // Get time id's from local storage as tagged in html
- $("#time8 .timeClass").val(localStorage.getItem("time8"));
- $("#time9 .timeClass").val(localStorage.getItem("time9"));
- $("#time10 .timeClass").val(localStorage.getItem("time10"));
- $("#time11 .timeClass").val(localStorage.getItem("time11"));
- $("#time12 .timeClass").val(localStorage.getItem("time12"));
- $("#time13 .timeClass").val(localStorage.getItem("time13"));
- $("#time14 .timeClass").val(localStorage.getItem("time14"));
- $("#time15 .timeClass").val(localStorage.getItem("time15"));
- $("#time16 .timeClass").val(localStorage.getItem("time16"));
- $("#time17 .timeClass").val(localStorage.getItem("time17"));
+ $("#time8 .scheduleItems").val(localStorage.getItem("time8"));
+ $("#time9 .scheduleItems").val(localStorage.getItem("time9"));
+ $("#time10 .scheduleItems").val(localStorage.getItem("time10"));
+ $("#time11 .scheduleItems").val(localStorage.getItem("time11"));
+ $("#time12 .scheduleItems").val(localStorage.getItem("time12"));
+ $("#time13 .scheduleItems").val(localStorage.getItem("time13"));
+ $("#time14 .scheduleItems").val(localStorage.getItem("time14"));
+ $("#time15 .scheduleItems").val(localStorage.getItem("time15"));
+ $("#time16 .scheduleItems").val(localStorage.getItem("time16"));
+ $("#time17 .scheduleItems").val(localStorage.getItem("time17"));
