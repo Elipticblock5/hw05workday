d71ff2a2a933c1713521438e44bbd28a74c76e59
NATE JOHNSON <42559281+Elipticblock5@users.noreply.github.com>
2021-03-06 10:49:40 -0700

    beggining date variable and set time to local storage to use later

---
+++ Assets/script.js
@@ -0,0 +1,42 @@
+ // Adding moment js
+
+ var dateNow = moment().format('MMMM Do YYYY, h:mm:ss a');
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ // Get time id's from local storage as tagged in html
+ $("#time8 .description").val(localStorage.getItem("time8"));
+ $("#time9 .description").val(localStorage.getItem("time9"));
+ $("#time10 .description").val(localStorage.getItem("time10"));
+ $("#time11 .description").val(localStorage.getItem("time11"));
+ $("#time12 .description").val(localStorage.getItem("time12"));
+ $("#time13 .description").val(localStorage.getItem("time13"));
+ $("#time14 .description").val(localStorage.getItem("time14"));
+ $("#time15 .description").val(localStorage.getItem("time15"));
+ $("#time16 .description").val(localStorage.getItem("time16"));
+ $("#time17 .description").val(localStorage.getItem("time17"));
