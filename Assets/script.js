ba3d32dbb56ac6eced26c0085a3f977bd8f6c2ab
NATE JOHNSON <42559281+Elipticblock5@users.noreply.github.com>
2021-03-06 11:09:47 -0700

    set hours to timeClass variable

--- Assets/script.js
+++ Assets/script.js
@@ -33,13 +33,13 @@
  
  
  // Get time id's from local storage as tagged in html
- $("#time8 .description").val(localStorage.getItem("time8"));
- $("#time9 .description").val(localStorage.getItem("time9"));
- $("#time10 .description").val(localStorage.getItem("time10"));
- $("#time11 .description").val(localStorage.getItem("time11"));
- $("#time12 .description").val(localStorage.getItem("time12"));
- $("#time13 .description").val(localStorage.getItem("time13"));
- $("#time14 .description").val(localStorage.getItem("time14"));
- $("#time15 .description").val(localStorage.getItem("time15"));
- $("#time16 .description").val(localStorage.getItem("time16"));
- $("#time17 .description").val(localStorage.getItem("time17"));
+ $("#time8 .timeClass").val(localStorage.getItem("time8"));
+ $("#time9 .timeClass").val(localStorage.getItem("time9"));
+ $("#time10 .timeClass").val(localStorage.getItem("time10"));
+ $("#time11 .timeClass").val(localStorage.getItem("time11"));
+ $("#time12 .timeClass").val(localStorage.getItem("time12"));
+ $("#time13 .timeClass").val(localStorage.getItem("time13"));
+ $("#time14 .timeClass").val(localStorage.getItem("time14"));
+ $("#time15 .timeClass").val(localStorage.getItem("time15"));
+ $("#time16 .timeClass").val(localStorage.getItem("time16"));
+ $("#time17 .timeClass").val(localStorage.getItem("time17"));
