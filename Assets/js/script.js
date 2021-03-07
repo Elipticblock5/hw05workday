0be17bcccff4d4613f4edb44d60521bfcd873137
NATE JOHNSON <42559281+Elipticblock5@users.noreply.github.com>
2021-03-07 12:37:29 -0700

    debugging coloring

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -17,14 +17,22 @@
         
         localStorage.setItem(timeNow, textNow);
     }) 
+
+    //function for color change
  
     function timeColor() {
         //this gets current number of hours
         var timeNowColor = moment().hour();
 
-        // this loops over time blocks to find past present future, using block time to match pre-filled CSS
+        console.log(timeNowColor)
+
+        // this loops over time blocks to find past present future, using block time to match pre-filled CSS, adding and appending 
+        // class with jquery to color from elements.
+
         $(".time-block").each(function () {
-            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+            var blockTime = parseInt($(this).attr("id"));
+
+            console.log(blockTime)
 
             // To check the time and add the classes for background indicators
             if (blockTime < timeNowColor) {
@@ -50,16 +58,16 @@
   
  
   // this grabs time id's as saved in local storage for coloring from above function
- $("#time8 .scheduleItems").val(localStorage.getItem("time8"));
- $("#time9 .scheduleItems").val(localStorage.getItem("time9"));
- $("#time10 .scheduleItems").val(localStorage.getItem("time10"));
- $("#time11 .scheduleItems").val(localStorage.getItem("time11"));
- $("#time12 .scheduleItems").val(localStorage.getItem("time12"));
- $("#time13 .scheduleItems").val(localStorage.getItem("time13"));
- $("#time14 .scheduleItems").val(localStorage.getItem("time14"));
- $("#time15 .scheduleItems").val(localStorage.getItem("time15"));
- $("#time16 .scheduleItems").val(localStorage.getItem("time16"));
- $("#time17 .scheduleItems").val(localStorage.getItem("time17")); 
+ $("#8 .scheduleItems").val(localStorage.getItem("8"));
+ $("#9 .scheduleItems").val(localStorage.getItem("9"));
+ $("#10 .scheduleItems").val(localStorage.getItem("10"));
+ $("#11 .scheduleItems").val(localStorage.getItem("11"));
+ $("#12 .scheduleItems").val(localStorage.getItem("12"));
+ $("#13 .scheduleItems").val(localStorage.getItem("13"));
+ $("#14 .scheduleItems").val(localStorage.getItem("14"));
+ $("#15 .scheduleItems").val(localStorage.getItem("15"));
+ $("#16 .scheduleItems").val(localStorage.getItem("16"));
+ $("#17 .scheduleItems").val(localStorage.getItem("17")); 
 
  timeColor ();
 })
